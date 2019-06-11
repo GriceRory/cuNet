9f60ebc76849754162bd0fe69f924fc0374365ea
Rory Grice <rory.grice01@gmail.com>
2019-06-11 14:59:00 +1200

    Update sample.cu
    small change to familiarize myself with the github application instead of website.

--- sample.cu
+++ sample.cu
@@ -3,3 +3,9 @@ struct sample{
   matrix output;
 };
 
+#include <sample.h>
+
+void buildSample(sample *s, int inputSize, int outputSize, int stride){
+  *s.input = buildMatrix(inputSize, 1, stride);
+  *s.output = buildMatrix(outputSize, 1, stride);
+}
