27d052687181a863ff5a892df8c1a84ae6aba361
rory <rory@rory-System-Product-Name>
2019-11-19 16:21:11 +1300

    the matrix methods are an abomination, need to write them so that shared memory is defined at compile time.

--- network.h
+++ network.h
@@ -1,8 +1,11 @@
-//core training
+//UTIL
 void train(network *n, database db);
 __global__ void backpropogate(network *n, sample *s);
+voi buildNetwork(network *n, int layers, int *nodes_in_layer);
 
-void runNetwork(network n, smaple *s);
+void runNetwork(network n, matrix input, matrix *output);
 void calculateLayer(matrix weights, matrix biases, matrix inputs, matrix *output);
 
+//signal functions and derivative calculators
 __device__ float sigmoid(float input);
+__device__ float sigmoidDerivative(float output);
